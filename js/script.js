63ef6b856479bd1ae29f7cf1a4ba02fe92837394
Cavid <111055564+JonyCavid@users.noreply.github.com>
2022-10-10 21:47:25 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".auto-type", {
+    strings: ["13 years old", "Coding", "  a Frontend Developer", "a Basketball Player", ],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
+
+
+var typed = new Typed(".auto-typing", {
+    strings: ["Thank you very much Hasan Eyvazoff for motivation to work as a programmer ❤", ],
+    typeSpeed: 150,
+    backSpeed: 150,
+})
+
+
